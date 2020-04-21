443fe0987aab862f74016b03aeae3ee43b5318e7
edz <edz@edzdeMacBook-Air.local>
2020-04-21 11:05:17 +0800

    4.21上午练习

---
+++ javadm/4.21上午练习.java
@@ -0,0 +1,16 @@
+class practice{
+public static void main(String[] args) {
+// float a = 3.402825F;//输出3.402825
+// 	System.out.println(a);
+// 	System.out.println((int)a);//输出3
+
+int a,b;
+a=0;
+b=0;
+boolean huo =false|(a++>0);
+System.out.println(a);
+
+boolean dlhuo = true||(b++>0);
+System.out.println(b);
+}
+}
