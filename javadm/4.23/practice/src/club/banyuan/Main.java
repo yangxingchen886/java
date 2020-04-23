4220f0c7d9178dd9633db9ed337edb85ca6e9299
edz <edz@edzdeMacBook-Air.local>
2020-04-23 20:45:14 +0800

    updata

---
+++ javadm/4.23/practice/src/club/banyuan/Main.java
@@ -0,0 +1,18 @@
+package club.banyuan;
+
+import club.banyuan.animal.*;
+import club.banyuan.human.*;
+
+public class Main {
+
+ public static void main(String[] args) {
+    Person person = new Person();
+    Cat cat = new Cat();
+    cat.name = "汤姆";
+    Dog dog = new Dog();
+    dog.setName("小强");
+    person.train(dog);
+    dog.playWith(cat);
+  }
+}
+
