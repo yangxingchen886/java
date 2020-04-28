f8f19dbbe1765ca0b45a9b6cef3c2c026b4c110c
yangxingchen886 <912821831@qq.com>
2020-04-28 20:30:00 +0800

    updata

---
+++ javadm/4.28/MainClass.java
@@ -0,0 +1,21 @@
+class One
+{
+    int x = 2121;
+}
+
+class Two
+{
+    int x = 1212;
+
+    {
+        System.out.println(x);
+    }
+}
+
+public class MainClass
+{
+    public static void main(String[] args)
+    {
+        Two two = new Two();
+    }
+}
