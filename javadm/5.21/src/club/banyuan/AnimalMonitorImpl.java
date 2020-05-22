d31156f724b6ebffc81aca60a8943ced64956f25
yangxingchen886 <912821831@qq.com>
2020-05-22 17:21:13 +0800

    updata

---
+++ javadm/5.21/src/club/banyuan/AnimalMonitorImpl.java
@@ -0,0 +1,197 @@
+package club.banyuan;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.w3c.dom.ls.LSOutput;
+
+/**
+ * 监视不同种类动物的数量。由观察者记录目击事件。
+ */
+public class AnimalMonitorImpl implements AnimalMonitor {
+
+  // 记录所有发现的动物。
+  private List<Sighting> sightings;
+
+  /**
+   * 创建一个AnimalMonitor。
+   */
+  public AnimalMonitorImpl() {
+    this.sightings = new ArrayList<>();
+  }
+
+
+  /**
+   * 打印所有目击动物的详细信息。
+   */
+  @Override
+  public void printList() {
+    for (int i = 0; i < sightings.size(); i++) {
+      System.out.println(sightings.get(i));
+    }
+
+  }
+
+  /**
+   * 打印给定动物的所有目击细节。
+   *
+   * @param animal 动物的类型。
+   */
+  @Override
+  public void printSightingsOf(String animal) {
+    for (int i = 0; i < sightings.size(); i++) {
+      if (animal == sightings.get(i).getAnimal()) {
+        System.out.println(sightings.get(i));
+      }
+
+    }
+  }
+
+  /**
+   * 打印给定观察者的所有目击动物。
+   *
+   * @param spotter 观察者的ID。
+   */
+  @Override
+  public void printSightingsBy(int spotter) {
+    for (int i = 0; i < sightings.size(); i++) {
+      if (spotter == sightings.get(i).getSpotter()) {
+        System.out.println(sightings.get(i));
+      }
+
+    }
+  }
+
+  /**
+   * 打印一份被视为濒临灭绝的动物的清单。
+   *
+   * @param animalNames     动物名称列表。
+   * @param dangerThreshold 小于或等于此级别的动物总数被认为濒临灭绝的
+   */
+  @Override
+  public void printEndangered(List<String> animalNames, int dangerThreshold) {
+
+    for (int i = 0; i < sightings.size(); i++) {
+      if (sightings.get(i).getCount() <= dangerThreshold) {
+        System.out.println(sightings.get(i));
+      }
+    }
+  }
+
+  /**
+   * 打印在特定期间periodID内记录的所有目击事件的详细信息，并将其作为参数传递给该方法
+   *
+   * @param period 日期ID
+   * @return 指定日期的清单
+   */
+  @Override
+  public List<Sighting> printSightingsInPeriod(int period) {
+    List<Sighting> ret = new ArrayList<>();
+    for (int i = 0; i < sightings.size(); i++) {
+      if (period == sightings.get(i).getPeriod()) {
+        System.out.println(sightings.get(i));
+        ret.add(sightings.get(i));
+      }
+    }
+    return ret;
+  }
+
+  /**
+   * 打印并返回指定日期区间内的清单
+   *
+   * @param fromPeriod 日期开始
+   * @param toPeriod   日期结束
+   * @param animal     动物类型
+   */
+  @Override
+  public List<Sighting> printSightingsOfInPeriod(int fromPeriod, int toPeriod, String animal) {
+    return null;
+  }
+
+  /**
+   * 打印特定动物类型的总的目击数量
+   *
+   * @param animal 动物类型
+   */
+  @Override
+  public List<Sighting> printCounts(String animal) {
+    int sum = 0;
+    for (int i = 0; i < sightings.size(); i++) {
+      if (animal == sightings.get(i).getAnimal()) {
+        sum += sightings.get(i).getCount();
+      }
+    }
+    System.out.println("目击数量："+sum);
+    return null;
+  }
+
+  /**
+   * 返回给定动物目击次数。
+   *
+   * @param animal 动物的类型。
+   * @return 给定动物的目击次数总数。
+   */
+  @Override
+  public int getCount(String animal) {
+    int sum = 0;
+    for (int i = 0; i < sightings.size(); i++) {
+      if (animal == sightings.get(i).getAnimal()) {
+        sum += sightings.get(i).getCount();
+      }
+    }
+    return sum;
+  }
+
+  /**
+   * 从发现清单中删除计数为零的记录。
+   */
+  @Override
+  public void removeZeroCounts() {
+
+  }
+
+  /**
+   * 返回特定区域内给定类型的动物的所有动物清单。
+   *
+   * @param animal 动物的类型。
+   * @param area   区域的ID。
+   * @return 目击清单。
+   */
+  @Override
+  public List<Sighting> getSightingsInArea(String animal, int area) {
+    return null;
+  }
+
+  /**
+   * 返回给定动物的所有目击清单。
+   *
+   * @param animal 动物的类型。
+   * @return 给定动物的所有目击物清单。
+   */
+  @Override
+  public List<Sighting> getSightingsOf(String animal) {
+    return null;
+  }
+
+  /**
+   * @param spotter
+   * @param period
+   * @return 包含该观察者在特定日期看到的动物的名称，只包括数量大于零的动物
+   */
+  @Override
+  public List<String> getAnimalBy(int spotter, int period) {
+    return null;
+  }
+
+  /**
+   * @param animal
+   * @param period
+   * @return 在该特定日期看到该动物的观察者
+   */
+  @Override
+  public List<Integer> getSpotterBy(String animal, int period) {
+    return null;
+  }
+
+
+}
