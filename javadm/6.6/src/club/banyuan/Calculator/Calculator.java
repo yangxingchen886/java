8bbe26d217b69490456c227235626d273de591a9
yangxingchen886 <912821831@qq.com>
2020-06-09 18:40:50 +0800

    updata

--- javadm/6.6/src/club/banyuan/Calculator/Calculator.java
+++ javadm/6.6/src/club/banyuan/Calculator/Calculator.java
@@ -1,65 +1,12 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class Calculator {
-
-  public static void main(String[] args) {
-    while (true){
-      Scanner sc = new Scanner(System.in);
-      System.out.println("请输入：");
-      String s = sc.nextLine();
-
-      if("quit".equals(s)){
-        System.exit(0);
-      }
-
-      String[] calculatorElements = s.split("\\s+");
-
-      if(calculatorElements.length != 3){
-        System.out.println("非法输入，请重新输入");
-        continue;
-      }
-
-      BigDecimal bigDecimal1 = new BigDecimal(calculatorElements[0]);
-      BigDecimal bigDecimal2 = new BigDecimal(calculatorElements[2]);
-
-      Pattern pattern1 = Pattern.compile("\\+");
-      Matcher matcher1 = pattern1.matcher(s);
-      if(matcher1.find()){
-        System.out.println(bigDecimal1.add(bigDecimal2));
-      }
-
-      Pattern pattern2 = Pattern.compile("-");
-      Matcher matcher2 = pattern2.matcher(s);
-      if(matcher2.find()){
-        System.out.println(bigDecimal1.subtract(bigDecimal2));
-      }
-
-      Pattern pattern3 = Pattern.compile("\\*");
-      Matcher matcher3 = pattern3.matcher(s);
-      if(matcher3.find()){
-        System.out.println(bigDecimal1.multiply(bigDecimal2));
-      }
-
-      Pattern pattern4 = Pattern.compile("/");
-      Matcher matcher4 = pattern4.matcher(s);
-      if(matcher4.find()){
-        System.out.println(bigDecimal1.divide(bigDecimal2,3, RoundingMode.HALF_UP));
-      }
-
-      Pattern pattern5 = Pattern.compile("%");
-      Matcher matcher5 = pattern5.matcher(s);
-      if(matcher5.find()){
-        BigDecimal[] bigDecimals = bigDecimal1.divideAndRemainder(bigDecimal2);
-        System.out.println(bigDecimals[1]);
-      }
-    }
-
-
-  }
-
-}
-
+public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("(\\d+(\\.\\d+)?)\\s*[+\\-*/%]\\s*(\\d+(\\.\\d+)?)");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                System.out.println(matcher.group(1));
+                System.out.println(matcher.group(3));
+            }else{
+                System.out.println("输入不合法，请重新输入");
+            }
